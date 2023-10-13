4b705dc644b460a27f47208a01357d59b9f4ce72
SantiagoAngel007 <1193571417@u.icesi.edu.co>
2023-10-12 23:16:42 -0500

    Add methods registerReminders, modifyReminders, deleteReminders and remindersInOrder

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,4 +1,121 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Controller {
+
+
+    ChainigHashTable<Integer,Reminder> hash;
+    PriorityQueue<Reminder> pQueue;
+    Queue<Reminder> queue;
+
+    ArrayList<Integer> keyArray;
+
+    public Controller(){
+        hash = new ChainigHashTable<>();
+        keyArray = new ArrayList<>();
+        queue = new Queue<>();
+        pQueue = new PriorityQueue<>();
+    }
+
+    public void testKeys(int key){
+        keyArray.add(key);
+    }
+
+
+    public <V> void priorityInOrder(){
+        if(!keyArray.isEmpty()){
+            //Reminder[] objectArray;
+            ArrayList<Reminder> priorityArray = new ArrayList<>();
+            for (int i = 0; i<keyArray.size();i++){
+                priorityArray.add(hash.hashSearch(keyArray.get(i)));
+                //System.out.println(priorityArray.get(i));
+            }
+            Reminder temp = null;
+            Integer temp2 = null;
+//            //int temp;
+            for(int i = 0; i<priorityArray.size();i++){
+                for (int j = 1; j<priorityArray.size()-i;j++){
+                    Reminder first = priorityArray.get(j-1);
+                    Reminder second = priorityArray.get(j);
+                    Reminder anterior = priorityArray.get(j-1);
+                    Reminder actual = priorityArray.get(j);
+//                    Integer firstKey = keyArray.get(j+1);
+//                    Integer secondKey = keyArray.get(j);
+//
+                    if(priorityArray.get(j-1)!=null && priorityArray.get(j)!=null){
+                        if(priorityArray.get(j-1).getPriority()>priorityArray.get(j).getPriority()){
+
+                            priorityArray.set(j,anterior);
+                            priorityArray.set(j-1,actual);
+                        }
+                    }
+                }
+            }
+            int count = 1;
+            for (int i = 0; i<priorityArray.size();i++){
+                System.out.println("The index of the element is: " + count);
+                System.out.println(priorityArray.get(i));
+                System.out.println("\n");
+                count++;
+            }
+        }
+    }
+
+
+    public boolean keyVerification(Integer key){
+        Integer processedKey = key;
+        boolean flag = false;
+        if(!keyArray.isEmpty()){
+            for (int i = 0; i<keyArray.size() && !flag;i++){
+                if (keyArray.get(i).compareTo(processedKey) == 0) {
+                    flag = true;
+                }
+            }
+        }
+        return flag;
+    }
+
+
+    public void registerReminders(String title, String description, String deadline, int priority, int key){
+        Reminder reminder = new Reminder(title,description,deadline,priority,key);
+        //Integer processedkey = key;
+        //int hashKey = hash.hashFuntion(processedkey);
+        hash.hashInsert(key, reminder);
+
+        if(reminder.getPriority()!=0){
+            pQueue.insert(reminder.getPriority(),reminder);
+        }else{
+            queue.enQueue(reminder);
+        }
+    }
+
+    public  void modifyReminders(String title, String description, String deadline, int priority, int key){
+        Reminder element = hash.hashSearch(key);
+        if(element!=null){
+
+            if(title!=null){
+                element.setTitle(title);
+            }
+            else if(description!=null){
+                element.setDescription(description);
+            }
+            else if(deadline!=null){
+                element.setDeadline(deadline);
+            }
+            else if(priority <= 5 && priority >= 1){
+                element.setPriority(priority);
+            }
+            else{
+                System.out.println("There is a problem");
+            }
+
+        }
+    }
+
+    public void deleteReminders(int key){
+        hash.hashDelete(key);
+    }
+
+
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,9 @@ package ui;
 
 import model.Controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -58,5 +61,176 @@ public class Main {
             default -> System.out.println("Error, wrong option");
         }
     }
+
+    //--------------------------------------------------------------------------------------\\
+
+
+    public int showHashMenu() {
+        int optionHash=0;
+
+        System.out.println(
+                "Game menu, please pick an option\n" +
+                        "(1) Create a reminder \n" +
+                        "(2) Modify reminder \n" +
+                        "(3) Delete reminder \n"+
+                        "(4) List by priority \n"+
+                        "(5) List by deadline \n"+
+                        "(0) Leave\n"
+        );
+        optionHash= sc.nextInt();
+        sc.nextLine();
+        return optionHash;
+    }
+
+    public void hashOperation(){
+
+        int optionHash=0;
+
+        do{
+            optionHash= showHashMenu();
+            executeOperationHash(optionHash);
+
+        }while (optionHash!=0);
+
+    }
+
+    public void executeOperationHash(int operation) {
+
+        switch(operation) {
+            case 0:
+                System.out.println("Returning to the tittle!");
+                break;
+            case 1:
+                registerTaskAndReminders();
+                break;
+            case 2:
+                modifyTaskAndReminders();
+                break;
+            case 3:
+                deleteTaskAndReminders();
+                break;
+            case 4:
+                cll.priorityInOrder();
+                break;
+            case 5:
+                System.out.println();
+                break;
+
+            default:
+                System.out.println("Error, wrong option");
+
+        }
+    }
+
+
+    public void registerTaskAndReminders(){
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-M-yyyy");
+
+        String actualDate = dateFormat.format(new Date());
+
+        System.out.println(actualDate);
+
+        System.out.println("Type the title of the reminder");
+        String title = sc.nextLine();
+        System.out.println("Type a description for the reminder");
+        String description = sc.nextLine();
+        System.out.println("Type the deadline of the reminder");
+        String deadline = sc.nextLine();
+
+
+        int key = 0;
+        boolean flag = true;
+        System.out.println("To save a reminder it is necessary to create a key for it");
+        while (flag){
+            key = sc.nextInt();
+            flag = cll.keyVerification(key);
+            if(flag){
+                System.out.println("The key already exist, type another one");
+            }
+        }
+
+        System.out.println("Does the reminder has a priority?");
+        String answer = sc.next();
+
+
+        int priority = 0;
+        if(answer.equals("yes")){
+            System.out.println("Type the priority of the reminder");
+            System.out.println(
+                    "How important the task or reminder is? " +
+                            "(1) Very important  \n" +
+                            "(2) Important  \n"+
+                            "(3) Considerable  \n"+
+                            "(4) Less important  \n"+
+                            "(5) Not very important \n"
+            );
+            priority = sc.nextInt();
+            cll.registerReminders(title,description,deadline,priority,key);
+        }else{
+            cll.registerReminders(title,description,deadline,priority,key);
+        }
+    }
+
+
+    public void modifyTaskAndReminders(){
+        System.out.println("To modify the reminder it is necessary to type the respective key");
+        int key = sc.nextInt();
+        int answer = -1;
+        String title;
+        String description;
+        String deadline;
+        int priority;
+
+
+        while(answer!=0) {
+            System.out.println(
+                    "What do you want to modify? \n" +
+                            "(1) Title  \n" +
+                            "(2) Description  \n" +
+                            "(3) Deadline  \n" +
+                            "(4) Priority  \n"
+            );
+            answer = sc.nextInt();
+
+            switch (answer) {
+                case 1:
+                    System.out.println("Type the new title for the reminder");
+                    sc.nextLine();
+                    title = sc.nextLine();
+                    cll.modifyReminders(title, null, null, 0, key);
+                    break;
+                case 2:
+                    System.out.println("Type the new description for the reminder");
+                    sc.nextLine();
+                    description = sc.next();
+                    cll.modifyReminders(null, description, null, 0, key);
+                    break;
+                case 3:
+                    System.out.println("Type the new deadline for the reminder");
+                    sc.nextLine();
+                    deadline = sc.next();
+                    cll.modifyReminders(null, null, deadline, 0, key);
+                    break;
+                case 4:
+                    System.out.println("Type the new priority for the reminder");
+                    priority = sc.nextInt();
+                    cll.modifyReminders(null, null, null, priority, key);
+                    break;
+
+                default:
+                    System.out.println("Error, wrong option");
+
+
+            }
+        }
+    }
+
+    public void deleteTaskAndReminders(){
+        System.out.println("To modify the reminder it is necessary to type the respective key");
+        int key = sc.nextInt();
+        cll.deleteReminders(key);
+    }
+
 
 }
